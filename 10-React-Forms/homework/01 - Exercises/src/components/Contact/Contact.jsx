c1f3521c5b133050704d7ada8d4402bd1a16c512
L-A-M-J-O <lamjo30@gmail.com>
2022-11-22 01:59:12 +0100

    Push

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { useState } from 'react';
 import './Contact.modules.css'
 
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export default function Contact () {
-  return <div>Crear Formulario</div>
+
+  const [state, setState ] = useState( 
+    {
+      name: '',
+      email: '',
+      phone: '',
+      problem: '',
+      mensaje: ''
+    }
+  )
+  
+  return <div>
+    <form>
+      <label>Name</label>
+      <br/>
+      <input name='name'value={state.name} type='text' placeholder=' Escribe tu Name'/>
+      <br/>
+      <label>Email</label>
+      <br/>
+      <input name='email'value={state.email}type='text' placeholder=' Escribe tu Email'/>
+      <br/>
+      <label>Phone</label>
+      <br/>
+      <input name='phone'value={state.phone}type='number' placeholder=' Escribe tu Phone'/>
+      <br/>
+      <label>Problem</label>
+      <br/>
+      <input name='problem'value={state.problem}type='text' placeholder=' Escribe tu Problem'/>
+      <br/>
+      <label>Mensaje</label>
+      <br/>
+      <input name='mensaje'value={state.mensaje}type='text' placeholder=' Escribe tu Mensaje...'/>
+      <hr/>
+      <input name='name'type='submit'/>
+    </form>
+  </div>
 }
